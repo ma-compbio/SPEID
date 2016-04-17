1afab1471330a7ae28b637bb4310a6fa555f03ae
sss1 <sss1@andrew.cmu.edu>
2016-04-16 23:56:51 -0400

    Added openblas installation to cluster installation instructions.

--- cluster_installation_steps.sh
+++ cluster_installation_steps.sh
@@ -10,13 +10,13 @@ wget https://www.python.org/ftp/python/2.7.11/Python-2.7.11.tar.xz
 tar -xvf Python-2.7.11.tar.xz
 rm Python-2.7.11.tar.xz
 cd Python-2.7.11/
-./configure --enable-shared --prefix=/home/$USER/local
+./configure --enable-shared --prefix=$HOME/local
 make
 make install
 cd ..
 
 # point python to its shared libraries
-export LD_LIBRARY_PATH=/home/$USER/local/lib
+export LD_LIBRARY_PATH=$HOME/local/lib
 
 # make an alias to run the new python installation
 alias python2.7=$HOME/local/bin/python2.7
@@ -38,6 +38,16 @@ export HDF5_DIR=$HOME/local
 # fortunately, newer versions of Python come packaged with pip
 # this step takes a while (especially scipy)
 python2.7 -m pip install numpy scipy nose theano h5py keras
+
+git clone https://github.com/xianyi/OpenBLAS  
+cd OpenBLAS  
+make FC=gfortran
+make PREFIX=$HOME/local install
+cd ..
+
+# create a file called .theanorc with the following contents
+# [blas]
+# ldflags = -L$HOME/local/lib -lopenblas
 
 # The numpy test will probably fail after about 500 test.
 # This is OK (there is a bug in the test, regarding where f2py should be, due
