28655a032cdc62b8823b0d26e2b500b9a4b5ea5d
sss1 <sss1@andrew.cmu.edu>
2016-07-14 16:13:27 -0400

    Removed max_len check.

--- load_data.py
+++ load_data.py
@@ -3,12 +3,10 @@ import random as rand
 import warnings
 import sys
 
-max_len = sys.maxsize # reduce this to prevent out-of-memory errors during prediction
-
 # reads the specified file and returns a num_samples X 4 X 1000 array
 def load_and_format_data(f_name):
 
-    num_pairs = min(max_len, sum(1 for line in open(f_name))/4); # number of sample pairs
+    num_pairs = sum(1 for line in open(f_name))/4; # number of sample pairs
 
     Xs = np.zeros((num_pairs, 4, 1000)) # allocate space for output
     
@@ -19,11 +17,6 @@ def load_and_format_data(f_name):
 
             sample_type = line_num % 4;
             sample_num = line_num / 4;
-
-            if sample_num >= max_len:
-                warnings.warn("The data are very large. Truncating at " +
-                        str(max_len) + " samples to avoid memory errors.")
-                break
 
             Xs[sample_num, sample_type, :] = [int(x) for x in line];
 
